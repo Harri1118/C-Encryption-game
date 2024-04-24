161ae7386f4ab8899dbb9496fbc667713ec3d5e2
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2024-04-24 18:19:39 -0400

    Update findPath.c

--- findPath.c
+++ findPath.c
@@ -20,6 +20,23 @@ char *fin = strdup(Path);
 return fin;
 }
 
+bool isEndingResponse(char* input){
+	char* win = "HTTP/1.1 200 OK\r\ncontent-type: text/html; charset=UTF-8 \r\n\r\n<html><body>Congratulations! You solved it! <a href=\"crypt\">Another?</a></body></html>";
+	char* err = "HTTP/1.1 200 OK\r\ncontent-type: text/html; charset=UTF-8 \r\n\r\n<html><body>Game isn't initiated! Please <a href=\"crypt\">click this link to start it!</a></body></html>";
+	int len = strlen(input);
+	int winLen = strlen(win);
+	int errLen = strlen(err);
+	if(len == winLen){
+		if(strcmp(input, win) == 0)
+			return true;
+	}
+	if(len == errLen){
+		if(strcmp(input, err) == 0)
+			return true;
+	}
+	return false;
+}
+
 // pathExists filters the header so that the filepath can be extracted
 // from it. It then calls fileExists to validate whether the file
 // exists or not.
